1b9c53a1927851c5a4c2231b1f85edd1b57f2c73
heska075 <luigi12mansion15@gmail.com>
2026-08-01 10:21:15 +0200

    Updated to 11.19.0_1

--- js/game_version.js
+++ js/game_version.js
@@ -1,5 +1,5 @@
 // const GAME_VERSION = "null";
-const GAME_VERSION = "11.19.0.638_7320204";
+const GAME_VERSION = "11.19.0_1_7326113";
 
 // const TEST_VERSION = "null";
 const TEST_VERSION = null;
